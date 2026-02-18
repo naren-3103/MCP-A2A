a1e740a5ea6c9de89577058973b82e1ad096fcc4
Naren <narenmouria.g@gmail.com>
2026-02-18 12:01:37 +0530

    delete all files in branch

--- mcp_github/mcp_main/tools/document_reader.py
+++
@@ -1,8 +0,0 @@
-class DocumentReaderTool:
-    name = "read_document"
-    description = "Reads a text document from disk"
-
-    def run(self, input_data):
-        path = input_data["path"]
-        with open(path, "r", encoding="utf-8") as f:
-            return f.read()
